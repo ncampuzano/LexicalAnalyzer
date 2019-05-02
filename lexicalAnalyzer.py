2a0684139f6fea274d808352713fb479144a9ddd
Nicolás Campuzano <nicolas.campuzano@lantri.com>
2019-05-01 20:16:07 -0500

    Add SL keywords

---
+++ lexicalAnalyzer.py
@@ -0,0 +1,6 @@
+keywords = [
+  'and', 'constantes', 'hasta', 'matriz', 'paso', 'registro', 'sino', 'vector', 'archivo',
+  'desde', 'inicio', 'mientras', 'subrutina', 'repetir', 'tipos', 'caso', 'eval', 'lib',
+  'not', 'programa', 'retorna', 'var', 'const', 'fin', 'libext', 'or', 'ref', 'si', 'variables'
+]
+
