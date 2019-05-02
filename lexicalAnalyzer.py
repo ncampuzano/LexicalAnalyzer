0b4ea15b3bbbc318b26a40faf34a9b1620e266b3
Juan Alvarez <jmalvarezd@unal.edu.co>
2019-05-02 00:13:10 -0500

    Fixed some stuff and broke other stuff. It now detects ids (and more)

--- lexicalAnalyzer.py
+++ lexicalAnalyzer.py
@@ -1,12 +1,19 @@
 keywords = [
   "and", "constantes", "hasta", "matriz", "paso", "registro", "sino", "vector", "archivo",
   "desde", "inicio", "mientras", "subrutina", "repetir", "tipos", "caso", "eval", "lib",
-  "not", "programa", "retorna", "var", "const", "fin", "libext", "or", "ref", "si", "variables"
+  "not", "programa", "retorna", "var", "const", "fin", "libext", "or", "ref", "si", "variables",
+  
+  "dim", "imprimir", "cls", "leer", "set_ifs", "abs", "arctan", "ascii", "cos", "dec",
+  "eof", "exp", "get_ifs", "inc", "int", "log", "lower", "mem", "ord", "paramval",
+  "pcount", "pos", "random", "sec", "set_stdin", "set_stdout", "sin", "sqrt",
+  "str", "strdup", "strlen", "substr", "tan", "upper", "val",
+
+  
 ]
 
 def main():
-  f = open("test.txt", "r")
-  lines = f.readlines()
+  with open("test.txt", "r") as f:
+    lines = f.readlines()
   column = 0
   row = 0
   for line in lines:
@@ -15,14 +22,17 @@ def main():
     word = ""
     for letter in line:
       column += 1
-      if letter.isspace(): # If there is a space character
-        if word in keywords and isReserved:
-          print("<" + word + "," + str(row) + "," + str(column - len(word))  + ">")
-        isReserved = True
-        word = ""
-      elif letter.isalpha():
+      if isAlnumOrUnderscore(letter):
         word += letter
-      else:
-        isReserved = False
+      else: 
+        if word in keywords:
+          print("<" + word + "," + str(row) + "," + str(column - len(word))  + ">")
+        elif word:
+          print("<id," + word + "," + str(row) + "," + str(column - len(word))  + ">")
+        word = ""
+
+def isAlnumOrUnderscore(stringToCheck):
+  return stringToCheck.isalnum() or stringToCheck == "_"
+
 if __name__ == "__main__":  
   main()
